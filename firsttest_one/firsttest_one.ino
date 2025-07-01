0bd099c7a5bcbd19e6c11f218ca73ad102dc8da3
Ariv Pasad <ariv.pasad@gmail.com>
2025-07-01 13:55:05 -0700

    Detect_0-1

--- firsttest_one/firsttest_one.ino
+++ firsttest_one/firsttest_one.ino
@@ -5,7 +5,7 @@
 MPU6050 mpu;
 
 float totalG = 0;        // sum of all gs
-int numSample = 0;         // how many gs so far
+int numSample = 0;       // how many gs so far
 float threshold = 2.0;   // how much means a spike? (test to get good value)
 
 void setup() {
@@ -27,27 +27,31 @@ void loop() {
   int16_t ax, ay, az;
   mpu.getAcceleration(&ax, &ay, &az);
 
-// CALCULATE THE G FORCE
-
+  // CALCULATE THE G FORCE
   float gX = ax / 16384.0;
   float gY = ay / 16384.0;
   float gZ = az / 16384.0;
-
   float g = sqrt(gX * gX + gY * gY + gZ * gZ);  // Total g-force
-Serial.println(g);
-//SET UP FOR AVG CALUCATION
 
+  // SET UP FOR AVG CALUCATION
   totalG = totalG + g;      // add new g to total sum
-  numSample++;        // add 1 every time, for average calucation
+  numSample++;              // add 1 every time, for average calucation
 
-//CALCULATE AVG
+  // CALCULATE AVG
   float avg = totalG / numSample;  // calculate the average
 
-//TELL SPIKE HAPPEND
+  // TELL SPIKE HAPPEND
+  int detect = 0;                 
   if (g > avg + threshold) {
-    Serial.print(" CRASH DETECTED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ARIV IS SO GOOD AT CODING!!!!!!! ");
+    detect = 1;    
+    Serial.println("CRASH DETECTED ‼️‼️!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!‼️‼️");
+    delay(1000);
+  }
 
+  // 
+  Serial.print(g);             
+  Serial.print(",");           
+  Serial.println(detect);      
 
-  delay(1000);
-}
+  delay(50);  // Smooth graph (20Hz)
 }
