153dd38b8353661a98498a73247ca9180cef1870
Ariv Pasad <ariv.pasad@gmail.com>
2025-11-21 17:54:32 -0800

    Added arrays for multiple sensors
     made all variables an array so it is adaptable to multiple sensors

--- Piezo_first/Piezo_first.ino
+++ Piezo_first/Piezo_first.ino
@@ -1,72 +1,70 @@
-  const int piezoPin1 = A0;     // Piezo sensor 1 analog pin
-  const int piezoPin2 = A2;     // Piezo sensor 2 analog pin
+  const int numSensors = 6;
+  const int piezoPins[numSensors] = {A0, A2, A3, A4, A5, A6};     // Piezo sensor pins as array
+  float piezoValue[numSensors];
+  float totalValue[numSensors] = {0, 0, 0, 0, 0, 0};
+  float avg[numSensors];
 
-  float totalValue1 = 0;
-  float totalValue2 = 0;
+
+  //float totalValue1 = 0;
+  //float totalValue2 = 0;
   unsigned int numSample = 0;
   float threshold = 50.0;
   const int recordsamples = 90;
 
-    float sensor1Data[recordsamples];             
-    float sensor2Data[recordsamples]; 
+  float sensorData[numSensors][recordsamples];             
+  
 
   void setup() {
     Serial.begin(9600);
   }
 
   void loop() {
-    float piezoValue1 = analogRead(piezoPin1);// read from it and assign to piezoValue1
-    float piezoValue2 = analogRead(piezoPin2);// read from it and assign to piezoValue2
-
-    totalValue1 += piezoValue1; // saying totalvalue equals piezoval1+totalval
-    totalValue2 += piezoValue2;
-
-  
-    numSample++;
-    float avg1 = totalValue1 / numSample;
-    float avg2 = totalValue2 / numSample;
-
+   numSample++;
     
-    if (piezoValue1 > avg1 + threshold || piezoValue2 > avg2 + threshold) {
-      //Serial.println(" ");
-     // Serial.print(numSample);
-      //Serial.print("|");
-      //Serial.print(totalValue1);
-      //Serial.print("|");
-      //Serial.print(totalValue2);
-      //Serial.print("|");
-      //Serial.print(piezoValue1);
-      //Serial.print("|");
-      //Serial.print(piezoValue2);
-      //Serial.print("|");
-      //Serial.print(avg1);
-      //Serial.print("|");
-      //Serial.println(avg2);
+      for (int sIdx = 0; sIdx < numSensors; sIdx++) {
+      // Read piezo values into array ( piezo number = read from which pin ( piezoPins[sIdx] ) )
+      piezoValue[sIdx] = analogRead(piezoPins[sIdx]);
 
-     
+      // add up total values read from previous loop  
+      totalValue[sIdx] += piezoValue[sIdx];
 
-     // float sensor1Data[recordsamples];             
-      //float sensor2Data[recordsamples];             
+       // calculate average values 
+      avg[sIdx] = totalValue[sIdx] / numSample;
+    }// end of for loop over sIdx
+   
+// --------------------- end of reading and averaging piezo values --------------------- //
 
-      for (int i = 0; i < recordsamples ; i++) {     
-        sensor1Data[i] = analogRead(piezoPin1);  
-        sensor2Data[i] = analogRead(piezoPin2); 
+// start  
+
+    for (int sIdx = 0; sIdx < numSensors; sIdx++) {
+        if (piezoValue[sIdx] > avg[sIdx] + threshold) { // checking if piezo value is a peak / hit
+          for (int i = 0; i < recordsamples ; i++) {     
+            sensorData[sIdx][i] = analogRead(piezoPins[sIdx]);  // save all data from each sensor into sensorData array
+            delay(50);                                
+          } 
+          
+        }
+     }
+    
+
+
+    //if (piezoValue1 > avg1 + threshold || piezoValue2 > avg2 + threshold) {           
+
+      //for (int i = 0; i < recordsamples ; i++) {     
+
+        //sensor1Data[i] = analogRead(piezoPin1);  
+        //sensor2Data[i] = analogRead(piezoPin2); 
       // delay(50);                                
-      }
-
-      for (int i = 0; i < recordsamples; i++) {     
-        Serial.print(sensor1Data[i]); 
-        Serial.print(",");
-        Serial.println(sensor2Data[i]);
+      
+      
+      }// end of full coid loop
     
         
       
-      }
-      //Serial.println();
+ 
+
 
                 
     
 
-  }
   
-  }
